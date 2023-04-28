1194219164ecd3bac98ad44044d4204c6e08db2b
Shivkumar122 <131873605+Shivkumar122@users.noreply.github.com>
2023-04-28 15:47:22 +0530

    Add files via upload

--- src/main/java/com/example/model/Book.java
+++ src/main/java/com/example/model/Book.java
@@ -20,7 +20,8 @@ public class Book {
     private String email;
     private String phone;
     private String lastName;
-    private  String firstName;
+    private String firstName;
+    private String city;
 
 
 
