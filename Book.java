81cf12ff1676e85e4f98aef62a8564d1898e1878
Shivkumar122 <131873605+Shivkumar122@users.noreply.github.com>
2023-04-28 15:33:04 +0530

    new change

---
+++ Book.java
@@ -0,0 +1,27 @@
+package com.example.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Data
+@ToString
+@Document(collection = "book")
+public class Book {
+
+    @Id
+    private String id;
+    private String name;
+    private String author;
+    private String email;
+    private String phone;
+    private String lastName;
+    private  String firstName;
+
+
+
+}
